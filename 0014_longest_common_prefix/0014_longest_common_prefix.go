fd24d41c52f399c43181b0b68edcf6d6076f4469
Erispyu <erispyu@gmail.com>
2022-08-07 17:57:29 +0800

    feat: 14 vertical scan sloution

--- 0014_longest_common_prefix/0014_longest_common_prefix.go
+++ 0014_longest_common_prefix/0014_longest_common_prefix.go
@@ -2,10 +2,27 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	fmt.Println()
 }
 
 func longestCommonPrefix(strs []string) string {
-	return ""
+	if strs == nil || len(strs) == 0 {
+		return ""
+	}
+
+	if len(strs) == 1 {
+		return strs[0]
+	}
+
+	for i := range strs[0] {
+		c := strs[0][i]
+		for j := 1; j < len(strs); j++ {
+			if i >= len(strs[j]) || c != strs[j][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+
+	return strs[0]
 }
