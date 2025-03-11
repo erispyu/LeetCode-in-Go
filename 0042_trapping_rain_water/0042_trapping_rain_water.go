7d8d1a842704f8548e86b820ec34559280482421
Erispyu <erispyu@gmail.com>
2025-03-11 20:55:41 +0800

    update

--- 0042_trapping_rain_water/0042_trapping_rain_water.go
+++ 0042_trapping_rain_water/0042_trapping_rain_water.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func trap(height []int) int {
 	if len(height) == 0 {
 		return 0
@@ -23,4 +25,24 @@ func trap(height []int) int {
 		}
 	}
 	return res
+}
+
+func main() {
+	// [0,1,0,2,1,0,1,3,2,1,2,1]
+	// 6
+	testCases := []struct {
+		height []int
+		want   int
+	}{
+		{[]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}, 6},
+		{[]int{4, 2, 0, 3, 2, 5}, 9},
+	}
+	for _, tc := range testCases {
+		got := trap(tc.height)
+		if got != tc.want {
+			fmt.Printf("failed: %v, %d, %d\n", tc.height, got, tc.want)
+		} else {
+			fmt.Printf("passed: %v, %d\n", tc.height, got)
+		}
+	}
 }
