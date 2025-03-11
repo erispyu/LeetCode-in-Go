2ad8db173a8ddadd1faf090304afcc0977c09b1b
Erispyu <erispyu@gmail.com>
2025-03-11 19:36:47 +0800

    update

--- 0015_three_sum/0015_three_sum.go
+++ 0015_three_sum/0015_three_sum.go
@@ -53,6 +53,38 @@ func binarySearch(nums []int, left, right, val int) int {
 	return binarySearch(nums, mid+1, right, val)
 }
 
+func threeSumOfficialSolSpeedUp(nums []int) (ans [][]int) {
+	sort.Ints(nums)
+	n := len(nums)
+	for i, x := range nums[:n-2] {
+		if i > 0 && x == nums[i-1] { // 跳过重复数字
+			continue
+		}
+		if x+nums[i+1]+nums[i+2] > 0 { // 优化一
+			break
+		}
+		if x+nums[n-2]+nums[n-1] < 0 { // 优化二
+			continue
+		}
+		j, k := i+1, n-1
+		for j < k {
+			s := x + nums[j] + nums[k]
+			if s > 0 {
+				k--
+			} else if s < 0 {
+				j++
+			} else {
+				ans = append(ans, []int{x, nums[j], nums[k]})
+				for j++; j < k && nums[j] == nums[j-1]; j++ {
+				} // 跳过重复数字
+				for k--; k > j && nums[k] == nums[k+1]; k-- {
+				} // 跳过重复数字
+			}
+		}
+	}
+	return
+}
+
 func main() {
 	nums := []int{-1, 0, 1, 2, -1, -4}
 	fmt.Println(threeSum(nums))
